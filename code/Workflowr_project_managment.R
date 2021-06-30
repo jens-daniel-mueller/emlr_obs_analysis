48723852d33c9c3d577494e05ff57bf6c1f8ff2b
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-06-30 11:05:15 +0200

    manual commit

---
+++ code/Workflowr_project_managment.R
@@ -0,0 +1,30 @@
+# This script summarizes the central commands and steps to set-up and organize a R project
+# using the Workflowr package.
+# For details please refer to:
+# https://jdblischak.github.io/workflowr/articles/wflow-01-getting-started.html
+
+
+# commit regular changes (locally) and rebuild site
+wflow_publish(all = TRUE, message = "additive era budgets")
+
+# commit changes including _site.yml (locally) and rebuild site
+wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
+
+# commit changes including _site.yml (locally) and rebuild site in the specified order
+wflow_publish(here::here(
+  "analysis",
+  c(
+    "index.Rmd",
+    "Gruber_2019_comparison.Rmd",
+    "column_inventories.Rmd",
+    "anomalous_changes.Rmd",
+    "publication.Rmd"
+  )
+),
+message = "include test on anomlous changes",
+republish = TRUE)
+
+
+# Push latest version to GitHub
+wflow_git_push()
+jens-daniel-mueller
