c11f8cd571c865d73647a0c36944cd9c522dbb14
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-07-29 16:13:55 +0200

    update regional budget analysis

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "update global budget analysis")
+wflow_publish(all = TRUE, message = "update regional budget analysis")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
