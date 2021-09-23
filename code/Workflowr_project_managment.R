9b1df4d25416a3c181f872127562f7878cc2a926
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-09-23 09:15:24 +0200

    1994 vs 2014 added

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "new vif analysis added")
+wflow_publish(all = TRUE, message = "1994 vs 2014 added")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
