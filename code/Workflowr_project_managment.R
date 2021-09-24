6a79fd0b61f80940e23df04ddc18230a6c6ddf33
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-09-24 16:54:19 +0200

    consider ensemble selection across all analysis

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised ensemble selection")
+wflow_publish(all = TRUE, message = "consider ensemble selection across all analysis")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
