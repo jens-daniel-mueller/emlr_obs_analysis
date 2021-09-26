ed889320b748c514d20e9565e54dd6c435cfbbee
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-09-26 17:35:06 +0200

    filter MLR basins

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "consider ensemble selection across all analysis")
+wflow_publish(all = TRUE, message = "filter MLR basins")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
