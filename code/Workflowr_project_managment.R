78424ad878f72270f6a4a45aa570a8ac208e9530
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-09-28 12:20:22 +0200

    update budget plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "compare direct and summed two decadel changes")
+wflow_publish(all = TRUE, message = "update budget plots")
 
 # commit changes including _site.yml (locally) and rebuild site
 wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
