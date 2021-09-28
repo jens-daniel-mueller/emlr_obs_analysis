ae39df229f090a1b6e63ce7e10587be97e11326e
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-09-28 16:37:26 +0200

    update density distribution

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,10 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "update map color scale range")
-
-t changes including _site.yml (locally) and rebuild site
-wflow_publish(c("analysis/*Rmd"), message = "XXX", republish = TRUE)
+wflow_publish(all = TRUE, message = "update density distribution")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here("analysis",
