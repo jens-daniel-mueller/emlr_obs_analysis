09c80f95b34905f1886801c8548be697a629f41d
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-10-26 10:07:56 +0200

    revised composite plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised composite plot")
+wflow_publish(all = TRUE, message = "revised composite plots")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here("analysis",
