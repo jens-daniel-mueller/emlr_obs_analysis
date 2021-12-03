43a45a79a77668917c97db79ad793910bb4d7354
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2021-12-03 22:00:23 +0100

    added gap filling coverage map

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "revised gap filling analysis")
+wflow_publish(all = TRUE, message = "added gap filling coverage map")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here("analysis",
