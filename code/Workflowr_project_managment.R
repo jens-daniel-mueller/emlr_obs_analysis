70a9b0173a240e051de5bf8e3c9dd204286dde23
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-12 21:57:15 +0100

    added maps with divergent color scale

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "code for coverage maps included")
+wflow_publish(all = TRUE, message = "added maps with divergent color scale")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
