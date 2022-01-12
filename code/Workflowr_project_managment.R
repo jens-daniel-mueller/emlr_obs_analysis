53dee502a9d69fd2fcee2bb9843488b4e424d33e
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-12 10:20:47 +0100

    rebuild with correct config

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,58 +5,39 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added gap filling coverage map")
+wflow_publish(all = TRUE, message = "rebuild with correct config")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
-wflow_publish(here::here("analysis",
-                         c("index.Rmd",
-                           "basics.Rmd",
-                           "cstar_correction_budgets.Rmd",
-                           "cstar_correction_column_inventories.Rmd",
-                           "cstar_correction_zonal_sections.Rmd",
-                           "cstar_scatter_budgets.Rmd",
-                           "cstar_scatter_column_inventories.Rmd",
-                           "cstar_scatter_zonal_sections.Rmd",
-                           "G19_budgets.Rmd",
-                           "G19_column_inventories.Rmd",
-                           "G19_zonal_sections.Rmd",
-                           "indian_test_budgets.Rmd",
-                           "indian_test_column_inventories.Rmd",
-                           "indian_test_zonal_sections.Rmd",
-                           "bottomdepth_budgets.Rmd",
-                           "bottomdepth_column_inventories.Rmd",
-                           "bottomdepth_zonal_sections.Rmd",
-                           "no_p_budgets.Rmd",
-                           "no_p_column_inventories.Rmd",
-                           "no_p_zonal_sections.Rmd",
-                           "no_n_budgets.Rmd",
-                           "no_n_column_inventories.Rmd",
-                           "no_n_zonal_sections.Rmd",
-                           "gaps_filter_budgets.Rmd",
-                           "gaps_filter_column_inventories.Rmd",
-                           "gaps_filter_zonal_sections.Rmd",
-                           "slab_budgets.Rmd",
-                           "slab_column_inventories.Rmd",
-                           "slab_zonal_sections.Rmd",
-                           "classic_budgets.Rmd",
-                           "classic_column_inventories.Rmd",
-                           "classic_zonal_sections.Rmd",
-                           "canyon_b_cleaning_overview.Rmd",
-                           "canyon_b_cleaning_budgets.Rmd",
-                           "canyon_b_cleaning_column_inventories.Rmd",
-                           "canyon_b_cleaning_zonal_sections.Rmd",
-                           "global_MLR_cleaning_budgets.Rmd",
-                           "global_MLR_cleaning_column_inventories.Rmd",
-                           "global_MLR_cleaning_zonal_sections.Rmd",
-                           "rarefication_budgets.Rmd",
-                           "rarefication_column_inventories.Rmd",
-                           "rarefication_zonal_sections.Rmd",
-                           "vif_budgets.Rmd",
-                           "vif_column_inventories.Rmd",
-                           "vif_zonal_sections.Rmd"
-                           )),
-              message = "added cstar correction analysis",
-              republish = TRUE)
+wflow_publish(here::here(
+  "analysis",
+  c(
+    "index.Rmd",
+    "gaps_filter_budgets.Rmd",
+    "gaps_filter_column_inventories.Rmd",
+    "gaps_filter_zonal_sections.Rmd",
+    "MLR_predictor_nitrate_budgets.Rmd",
+    "MLR_predictor_nitrate_column_inventories.Rmd",
+    "MLR_predictor_nitrate_zonal_sections.Rmd",
+    "basics.Rmd",
+    "classic_budgets.Rmd",
+    "classic_column_inventories.Rmd",
+    "classic_zonal_sections.Rmd",
+    "G19_budgets.Rmd",
+    "G19_column_inventories.Rmd",
+    "G19_zonal_sections.Rmd",
+    "indian_test_budgets.Rmd",
+    "indian_test_column_inventories.Rmd",
+    "indian_test_zonal_sections.Rmd",
+    "MLR_predictor_budgets.Rmd",
+    "MLR_predictor_column_inventories.Rmd",
+    "MLR_predictor_zonal_sections.Rmd",
+    "MLR_target_budgets.Rmd",
+    "MLR_target_column_inventories.Rmd",
+    "MLR_target_zonal_sections.Rmd"
+  )
+),
+message = "rebuild without any gap filter or flagging exceptions",
+republish = TRUE)
 
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
@@ -68,7 +49,7 @@ wflow_publish(here::here(
     "classic_zonal_sections.Rmd"
   )
 ),
-message = "test child document")
+message = "split child documents")
 
 
 # Push latest version to GitHub
