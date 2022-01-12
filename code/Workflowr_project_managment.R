9aff2078763fe78cf8bad0c710be140259fd14ee
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-12 22:25:39 +0100

    added sections with divergent color scale

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added maps with divergent color scale")
+wflow_publish(all = TRUE, message = "added sections with divergent color scale")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
