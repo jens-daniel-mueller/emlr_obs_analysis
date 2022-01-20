416e1076a9015d54099eb310be7505ea1be136e5
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-20 15:02:13 +0100

    added delta dcant map

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "updated alluvial plots")
+wflow_publish(all = TRUE, message = "added delta dcant map")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
