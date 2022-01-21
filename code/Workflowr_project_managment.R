0379e99e6ead9b0c42bdad6c06dc3bbf2cdafecd
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-21 11:58:11 +0100

    script cleaning

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "added map of scaled absolute change")
+wflow_publish(all = TRUE, message = "script cleaning")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
