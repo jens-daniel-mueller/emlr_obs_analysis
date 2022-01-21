da17a075575f1470cf435339921c302c5234f913
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-01-21 15:15:05 +0100

    added mean concentrations profiles

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "run color map test")
+wflow_publish(all = TRUE, message = "added mean concentrations profiles")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
