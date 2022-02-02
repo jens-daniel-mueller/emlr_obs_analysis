a332fe6ec6a85d2bb6d5434b693b2204ba5dff63
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-02-02 22:11:06 +0100

    ensemble uncertainties in time series

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "incl ensemble uncertainties in plot")
+wflow_publish(all = TRUE, message = "ensemble uncertainties in time series")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
