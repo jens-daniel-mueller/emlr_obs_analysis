5cc5572c8294362b108d5dd44dac049af4c76e39
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-02-02 12:40:00 +0100

    included Sabine column inventory as reference

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "plot 5 basin map")
+wflow_publish(all = TRUE, message = "included Sabine column inventory as reference")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
