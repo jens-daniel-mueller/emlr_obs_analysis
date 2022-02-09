e281bf389f7c19bf92f1ba99d0376ef68be52bc0
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-02-09 01:06:01 +0100

    updated plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "cleaned plots")
+wflow_publish(all = TRUE, message = "updated plots")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
