9ea0d8c4aec0107c12b38b733a5dce59efc6756e
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-02-17 13:01:23 +0100

    adapted beta factor

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "scaled budgets to global coverage")
+wflow_publish(all = TRUE, message = "adapted beta factor")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
