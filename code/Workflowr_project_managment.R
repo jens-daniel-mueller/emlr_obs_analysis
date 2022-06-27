dd2063a6e18b6cc61da0fff28447c00881195e7e
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-27 13:23:46 +0200

    ensemble member analysis added

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "1 as standard case and new ensemble members")
+wflow_publish(all = TRUE, message = "ensemble member analysis added")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
