6e173bfaabf9d4dffe3e9d5546fb474c484accec
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-06-30 16:30:45 +0200

    updated regional budget plots

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "ensemble with unadjusted data")
+wflow_publish(all = TRUE, message = "updated regional budget plots")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
