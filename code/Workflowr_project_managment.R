7f85db34e404c8a8588beeaad7772e669f831739
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-07-01 16:44:04 +0200

    added basin separation analysis

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "cleaned drift assesment")
+wflow_publish(all = TRUE, message = "added basin separation analysis")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
