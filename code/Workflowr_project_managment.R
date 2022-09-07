1024099f789920b64c04b902f9131406660a587f
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-09-07 08:02:13 +0200

    testrun integration 1000

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "testrun integration 10000")
+wflow_publish(all = TRUE, message = "testrun integration 1000")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
