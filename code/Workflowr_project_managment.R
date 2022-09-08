6f53c2857dced693debbb56acf1c471e5dc9a362
jens-daniel-mueller <jensdaniel.mueller@usys.ethz.ch>
2022-09-08 09:19:21 +0200

    column inventory uncertainty stippling added

--- code/Workflowr_project_managment.R
+++ code/Workflowr_project_managment.R
@@ -5,7 +5,7 @@
 
 
 # commit regular changes (locally) and rebuild site
-wflow_publish(all = TRUE, message = "testrun integration 3000")
+wflow_publish(all = TRUE, message = "column inventory uncertainty stippling added")
 
 # commit changes including _site.yml (locally) and rebuild site in the specified order
 wflow_publish(here::here(
